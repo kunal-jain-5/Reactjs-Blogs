cd19184404f1931bf869fe8fddd0c4f2b2abb5ef
Kunal Jain <jkunal526@gmail.com>
2021-07-08 22:03:41 +0530

    change

---
+++ src/BlogList.js
@@ -0,0 +1,19 @@
+const BlogList = (props) => {
+    const blogs = props.blogprop
+    const title = props.title
+    const deleteHandle = props.deleteHandle
+    return (  
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={() => deleteHandle(blog.id)}>Delete Blog</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
