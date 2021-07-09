a2071264501bef876526bd64936630809eac6a6e
Kunal Jain <jkunal526@gmail.com>
2021-07-09 23:23:39 +0530

    error message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,32 @@ import BlogList from "./BlogList";
 const Home = () => {
     
     const [blogs, setBlogs] = useState(null)
-
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
         .then(Response => {
+            if (!Response.ok){
+                throw Error("could not fetch data")
+            }
             return Response.json()
         })
         .then((data) => {
             setBlogs(data)
+            setIsPending(false)
+            setError(null)
+        })
+        .catch(err => {
+            setError(err.message)
+            setIsPending(false)
         })
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogprop={blogs} title="All Blogs" />}
         </div>
       );
