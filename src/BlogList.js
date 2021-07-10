a394d5c69fbb6ae57f0087a31bda2370eefef30a
Kunal Jain <jkunal526@gmail.com>
2021-07-10 14:00:20 +0530

    added BlogDetails and used route parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const BlogList = (props) => {
     const blogs = props.blogprop
     const title = props.title
@@ -6,8 +7,10 @@ const BlogList = (props) => {
             <h2>{title}</h2>
             {blogs.map((blogItem) => (
                 <div className="blog-preview" key={blogItem.id}>
+                    <Link to={`/blogs/${blogItem.id}`}>
                     <h2>{ blogItem.title }</h2>
                     <p>Written by {blogItem.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
