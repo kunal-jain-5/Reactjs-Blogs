bd3e5d3461fcfc975e54f6d81e0db44d069d9a47
Kunal Jain <jkunal526@gmail.com>
2021-07-11 16:17:46 +0530

    added create form and submit event

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [ title, setTitle ] = useState("")
+    const [ body, setBody ] = useState("")
+    const [ author, setAuthor] = useState("mario")
+
+    const submitHandle = (e) => {
+        e.preventDefault()
+        const blog = { title, body, author }
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form onSubmit={submitHandle}>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author:</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
